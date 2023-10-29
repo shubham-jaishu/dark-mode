1aba80b893032c409b577916589cff136168eb33
Shubham Kumar <109282657+shubham-jaishu@users.noreply.github.com>
2023-10-29 22:27:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let icon = document.querySelector("#icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "sun.png";
+    }
+    else{
+        icon.src = "moon.png";
+    }
+}
